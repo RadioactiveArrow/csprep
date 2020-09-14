a902739337cbbf070a886c59b752d3567054b30a
Sarvesh Chezhian <savvychez@gmail.com>
2020-09-13 21:34:29 -0500

    removed a dash

--- deploy.php
+++ deploy.php
@@ -1,5 +1,5 @@
 <?php
 // if ($_POST['payload']) {
-    shell_exec("cd /var/www/csprep/ && sudo git reset –-hard HEAD && sudo git pull");
+    shell_exec("cd /var/www/csprep/ && sudo git reset –hard HEAD && sudo git pull");
 // }
 
